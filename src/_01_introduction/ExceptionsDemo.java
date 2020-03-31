9fc0896547fb2c4414561a2f79b61a49da4ab3f3
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2020-03-31 14:03:54 -0700

    e

--- src/_01_introduction/ExceptionsDemo.java
+++ src/_01_introduction/ExceptionsDemo.java
@@ -15,29 +15,58 @@ public class ExceptionsDemo {
 		//    call to testMethod1 with a try/catch block.
 		//    In the catch block, put a print statement so you know
 		//    if the code is being ran.
-		
+		try {
+			testMethod1(2);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// 2. Now call testMethod1 with a value greater than 5. 
 		//    Running your program should not print the statement
 		//    in the catch block.
 		//    You can use the Exception class's printStackTrace() method
 		//    to print a list of methods that are currently in scope.
-		
+		try {
+			testMethod1(20);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// 3. Create a new class called CustomException that extends Exception.
 		//    Add a void method called terminate that simply calls System.exit(0);
-		
+		class CustomException{
+			
+			
+			void terminate() {
+				System.exit(0);
+			}
+		}
 		// 5. Call testMethod2 with a negative number in a try/catch block.
 		//    In the catch block, call the terminate method using the 
 		//    CustomException object.
 		//    This demonstrates how custom exception classes can be used to
 		//    handle exceptions in unlimited ways.
+		try {
+			testMethod2(-1);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			
+		}
 	}
 	
 	public static void testMethod1(int x) throws Exception {
 		if(x < 5) {
 			throw new Exception();
+			
 		}
 	}
 	
 	// 4. Create a static void method called testMethod2(int x) that throws
 	//    a CustomException if x is negative.
+	static void testMethod2(int x) throws Exception {
+		if(x < 0)
+			throw new Exception();
+			new CustomException().terminate();
+	}
 }
